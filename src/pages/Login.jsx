7aea0c5bb2ce5eab1dc07bb6ac98a4d72309bde5
unknown <luizgdsieira@gmail.com>
2025-09-08 23:23:12 -0300

    novatua60

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,19 +19,19 @@ export default function Login() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      {/* Div da imagem */}
-      <div className="flex-1 hidden md:block">
+    <div className="flex min-h-screen">
+      {/* Coluna da imagem */}
+      <div className="hidden md:flex flex-1">
         <img
-          src="/images/eduonback.png"
+          src="/images/eduonback.png" // imagem já cortada para evitar áreas brancas
           alt="Background"
-          className="object-cover w-full h-full"
+          className="object-cover h-full"
         />
       </div>
 
-      {/* Div do login */}
+      {/* Coluna do login */}
       <div className="flex-1 flex justify-center items-center bg-gray-900">
-        <div className="w-full max-w-md p-8">
+        <div className="h-50 max-w-md p-8">
           <h1 className="text-3xl font-bold mb-4 text-white">
             EDUON - Gestão Educacional
           </h1>
@@ -68,6 +68,76 @@ export default function Login() {
 }
 
 
+/*
+import { useState } from "react";
+import api from "../api/axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post("/auth/login", { username, password });
+      localStorage.setItem("token", res.data.token);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login inválido");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+       
+      <div className="flex-1 hidden md:block">
+        <img
+          src="/images/eduonback.png"
+          alt="Background"
+          className="object-cover w-full h-full"
+        />
+      </div>
+
+    /*
+      <div className="flex-1 flex justify-center items-center bg-gray-900">
+        <div className="w-full max-w-md p-8">
+          <h1 className="text-3xl font-bold mb-4 text-white">
+            EDUON - Gestão Educacional
+          </h1>
+          <p className="mb-6 text-gray-300">
+            Gerencie alunos e otimize processos.
+          </p>
+
+          <form onSubmit={handleLogin} className="space-y-4">
+            <input
+              type="text"
+              placeholder="Usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full p-3 bg-gray-800/80 border-none rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-3 bg-gray-800/80 border-none rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white p-3 rounded-lg hover:bg-blue-700 transition"
+            >
+              Acessar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+*/
 
 /*
 import { useState } from "react";
