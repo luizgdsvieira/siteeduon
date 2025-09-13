41515bf62bfb2e4758294f9e76d985135294ecfe
unknown <luizgdsieira@gmail.com>
2025-09-13 13:05:22 -0300

    novatua74

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
         <img
           src="/images/eduonback.png"
           alt="Background"
-          className="w-1/2 h-screen object-cover"
+          className="w-1/2 h-full object-cover"
         />
       </div>
       {/* Coluna do login */}
