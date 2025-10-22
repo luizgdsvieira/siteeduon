ea1d8b72a13de3b620113557184a05c882b5ea62
unknown <luizgdsieira@gmail.com>
2025-10-21 21:46:28 -0300

    atual-api-site-50

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -108,7 +108,8 @@ export default function Login() {
               backgroundColor: "#0A1128",
               color: "#ffffff",
               borderRadius: "12px",
-              paddingTop: "10px",
+              paddingTop: "20px",
+              padding: "10px",
               boxShadow: "0 8px 30px rgba(10, 17, 40, 0.25)",
               boxSizing: "border-box",
             }}
